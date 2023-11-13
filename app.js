365f24beb7807df6d7586706d6402fc20be8afc2
jaganpv <98324853+jaganpv@users.noreply.github.com>
2023-11-13 18:06:17 +0530

    Update app.js
    express

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+
+var express = require("express");
+
+var app = express();
+
+
+var server = app.listen(9090, function(){
+    var port = server.address().port;
+    console.log("Server started at http://localhost:%s", port);
+});
+
 console.log("Hello world js !!!");
